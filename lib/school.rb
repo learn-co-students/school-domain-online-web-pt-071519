932ce672f2e9d06f8bda4ff88429788ef37f0b8f
assane savadogo <assane.savadogo81@gmail.com>
2019-08-04 23:14:45 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
-# code here!
+require 'pry'
+class School
+  
+  attr_reader :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
+  
+  def grade(grade)
+    @roster.each do |key, value|
+      if key == grade
+        return value
+      #binding.pry
+      end
+    end
+  end
+  
+  def sort 
+    new_hash = {}
+    @roster.each do |key, value|
+      new_hash[key] = value.sort!
+    end
+    new_hash
+  end
+  
+end
