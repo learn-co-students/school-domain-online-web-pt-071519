0d20ac774818550e0cc034fba07090aeb455dc39
Broderick  Fowler Jr <broderickfowler@outlook.com>
2019-08-11 18:59:54 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
 # code here!
+class School
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def roster
+    @roster
+  end
+  
+  def add_student(name,grade)
+      if @roster[grade]
+      @roster[grade] << name
+    else
+      roster[grade] = [name]
+    end
+    
+    def grade(grade)
+      self.roster[grade]
+    end
+    
+  def sort
+    self.roster.each do |grade, students|
+      students.sort!
+     end
+    end
+  end
+end
+
+    
+    
